15906a64551fab4ea411bd119e978b877cde505b
Dmitriy Laykom <laykomdn@yandex.ru>
2022-08-06 08:59:59 +0700

    Создайте VPC с подсетями в разных зонах доступности

--- infra/main.tf
+++ infra/main.tf
@@ -56,6 +56,13 @@ resource "yandex_vpc_subnet" "subnet-1" {
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
+resource "yandex_vpc_subnet" "subnet-2" {
+  name		 = "subnet2"
+  zone 		 = "ru-central1-b"
+  network_id	 = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.20.0/24"]
+}
+
 output "internal_ip_address_vm_1" {
   value = yandex_compute_instance.vm-1.network_interface.0.ip_address
 }
